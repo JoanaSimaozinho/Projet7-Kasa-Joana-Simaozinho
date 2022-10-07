e642c337092abda8acbfb3f7c80c1a5e38f234dc
joanasimaozinho <joanasmnho83310@gmail.com>
2022-10-07 18:11:25 +0200

    export des dossiers supplémentaire a l'index du dossier components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,11 @@
 export { default as Header } from "./Header/Header";
 export { default as Home } from "./Home/Home";
+export { default as Annonces } from "./Home/Annonces/Annonces";
 export { default as FicheLogement } from "./FicheLogement/FicheLogement";
+export { default as Caroussel } from "./FicheLogement/Caroussel/Caroussel";
+export { default as Description } from "./FicheLogement/Description/Description";
+export { default as Titre } from "./FicheLogement/Titre/Titre";
+export { default as Equipements } from "./FicheLogement/Equipements/Equipements";
 export { default as APropos } from "./APropos/APropos";
 export { default as ErrorPage } from "./ErrorPage/ErrorPage";
 export { default as Footer } from "./Footer/Footer";
