a3843fa1bb58b01fd587805bc554b4aac7bbd7c0
joanasimaozinho <joanasmnho83310@gmail.com>
2022-10-08 10:27:09 +0200

    Ajout des fonts family ainsi que mise en forme du header et header css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,29 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div className="header">
+        <div>
+          <img src="../img/logo.svg" alt="logo" className="logo" />
+        </div>
+        <div className="linksHeader">
+          <div>
+            <Link to="/" className="link">
+              Accueil
+            </Link>
+          </div>
+          <div>
+            <Link to="/apropos" className="link">
+              A propos
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
