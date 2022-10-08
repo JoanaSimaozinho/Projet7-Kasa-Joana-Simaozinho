1ac57857ea95a26b13f87b452799c3bf9408f455
joanasimaozinho <joanasmnho83310@gmail.com>
2022-10-08 11:17:02 +0200

    Modification de App.js et renommage du dossier RoutesPages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { Home } from "./components";
+import { RoutesPages } from "./components";
 
 function App() {
   return (
     <div>
       <Home />
+      <RoutesPages />
     </div>
   );
 }
