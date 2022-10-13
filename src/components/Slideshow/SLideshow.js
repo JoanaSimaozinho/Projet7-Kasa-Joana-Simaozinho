babc652d4233c351c38076b371f684d2005306ed
joanasimaozinho <joanasmnho83310@gmail.com>
2022-10-13 10:54:08 +0200

    mise en forme du slider avec swiper prev et next et les bullets points

--- src/components/Slideshow/SLideshow.js
+++ src/components/Slideshow/SLideshow.js
@@ -1,35 +1,41 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 import "./Slideshow.css";
 
-import "bootstrap/dist/css/bootstrap.css";
-import Carousel from "react-bootstrap/Carousel";
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 
-function Slideshow() {
+export default function Slideshow() {
   return (
-    <div>
-      <div className="componentCarousel">
-        <Carousel>
-          <Carousel.Item>
-            <img className="slider" src="../../../img/logo.svg" alt="slider" />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="slider"
-              src="../../../img/backgroundimage.png"
-              alt="slider"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="slider"
-              src="../../../img/backgroundhome.png"
-              alt="slider"
-            />
-          </Carousel.Item>
-        </Carousel>
-      </div>
-    </div>
+    <>
+      <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        rewind={true}
+      >
+        <SwiperSlide>
+          <img src="../../../img/logo.svg" alt="photo" className="images" />
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
   );
 }
-
-export default Slideshow;
