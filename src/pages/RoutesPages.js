ff760b5783bb3c2f5af236ffeb9968b17752dae8
joanasimaozinho <joanasmnho83310@gmail.com>
2022-11-02 10:44:00 +0100

    commentaire sur le fichier routes

--- src/pages/RoutesPages.js
+++ src/pages/RoutesPages.js
@@ -1,3 +1,7 @@
+// Importation de React
+// Importation de Route et Routes à partir de react router dom
+// Importation des fichier Home, FicheLogement, APropos et ErrorPage à partir de l'index du dossierpages
+
 import * as React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home, FicheLogement, APropos, ErrorPage } from "./index";
@@ -5,6 +9,8 @@ import { Home, FicheLogement, APropos, ErrorPage } from "./index";
 function RoutesPages() {
   return (
     <div>
+
+      {/* Les routes permettent de naviguer entre les différentes pages du site web */}
       <Routes>
         {/* Route Home */}
         <Route path="/" element={<Home />} />
